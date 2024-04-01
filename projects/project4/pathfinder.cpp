ad20356488a0a3bc974d3acefd7c30aa818da23c
ILikeDogz <IanHaverstock@gmail.com>
2024-04-01 01:01:22 -0400

    save

--- projects/project4/pathfinder.cpp
+++ projects/project4/pathfinder.cpp
@@ -4,6 +4,27 @@
 #include "list.hpp"
 #include "queue.hpp"
 
+ void find_neighbors(int height, int width, int pos, int neighbors[], Image<Pixel> image){
+  //neighbors[0] = previous row, neighbors[1] = next row, neighbors[2] = previous col, neighbors[3] = next col
+  int row_offsets[4] = {-1, 1, 0, 0};
+  int col_offsets[4] = {0, 0, -1, 1};
+
+  //convert single int position to our row col data
+  int row = pos/width;
+  int col = pos%width;
+  for(int i = 0; i < 4; i++){
+    int new_row = row + row_offsets[i];
+    int new_col = col + col_offsets[i];
+
+    if(new_row < 0 || new_row >= height || new_col < 0 || new_col >= width || image(new_row, new_col) == BLACK){ //boundary check
+      neighbors[i] = -1; //invalid bounds
+    }
+    else {
+      neighbors[i] = new_row*width+new_col;
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
   // get input/output file names from command line arguments
@@ -18,11 +39,74 @@ int main(int argc, char *argv[])
   std::string output_file = argv[2];
 
   // Read input image from file
-  Image<Pixel> image = readFromFile(input_file);
-
+  Image<Pixel> image;
+  try{
+    image = readFromFile(input_file);
+  }catch(std::runtime_error& my_error){
+    std::cerr<<"Error: failed to readFromFile()"<<my_error.what()<<std::endl;
+    return EXIT_FAILURE;
+  }
   // TODO
+  int start_position = 0;
+  int height = image.height();
+  int width = image.width();
+  int red_found = 0;
+  for(int row = 0; row < height; row++){
+     for(int col = 0; col < width; col++){
+      bool is_red = image(row,col) == RED;
+      if(is_red){
+        start_position = row*width+col; //creates a unique single digit int that can be used to get the original values later
+        red_found++;
+      }
+      if(image(row, col) != RED && image(row, col) != WHITE && image(row, col) != BLACK){
+        std::cerr<<"Error: Invalid Color found inside Maze"<<std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+  }
+  if(red_found == 0 || red_found > 1){
+    std::cerr<<"Error: Incorrect number of red pixels found"<<std::endl;
+    return EXIT_FAILURE;
+  }
 
-  // Write solution image to file
-  writeToFile(image, output_file);
-  
+  Queue<int, List<int>> my_queue;
+
+  my_queue.enqueue(start_position);
+
+  // auto q = new Queue();
+  // q.add(inital_pos);
+  int my_neighbors[4] = {0, 0, 0, 0};
+  bool visited[height*width];
+  while(!my_queue.isEmpty()){
+    int current_position = my_queue.peekFront();
+    my_queue.dequeue();
+    find_neighbors(height, width, current_position, my_neighbors, image); //neighbors are now all stored in the array
+    for(int neighbor : my_neighbors){
+      if(neighbor == -1){
+        continue; //skip -1 as it means invalid neighbor
+      } else{
+        if(!visited[neighbor]){
+          my_queue.enqueue(neighbor);
+        }
+        visited[neighbor] = true;
+      }
+    }
+    //convert to row column data
+    int row = current_position/width;
+    int col = current_position%width;
+    if(row == 0 || row == height-1 || col == width-1 || col == 0){
+      std::cout<<"Solution Found"<<std::endl;
+      image(row, col) = GREEN;
+      // Write solution image to file
+      try{
+        writeToFile(image, output_file);
+      }catch(std::runtime_error& my_error){
+        std::cerr<<"Error: failed to writeToFile()"<<my_error.what()<<std::endl;
+        return EXIT_FAILURE;
+      }
+      return EXIT_SUCCESS;
+    }
+  }
+  std::cout<<"No Solution Found"<<std::endl;
+  return EXIT_SUCCESS;
 }
--- projects/project4/queue.tpp
+++ projects/project4/queue.tpp
@@ -3,26 +3,34 @@
 template <typename T, typename L>
 void Queue<T,L>::enqueue(const T& item)
 {
+  internal_list.insert(internal_list.getLength(), item);
   // TODO
 }
 
 template <typename T, typename L>
 void Queue<T,L>::dequeue()
 {
+  if(isEmpty()){
+    throw std::range_error("Error: queue is empty");
+  }
+  internal_list.remove(0);
   // TODO
 }
 
 template <typename T, typename L>
 T Queue<T,L>::peekFront()
 {
+  if(isEmpty()){
+    throw std::range_error("Error: queue is empty");
+  }
   // TODO
-  return T();
+  return internal_list.getEntry(0);
 }
 
 template <typename T, typename L>
 bool Queue<T,L>::isEmpty() const
 {
   // TODO
-  return false;
+  return internal_list.isEmpty();
 }
 
